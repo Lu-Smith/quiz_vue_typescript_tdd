b14bfc2c3d7f5bc67c138136e4ed60efb8d46dfd
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-06-19 19:13:50 +0100

    Updated unit test

--- tests/unit/example.spec.ts
+++ tests/unit/example.spec.ts
@@ -10,20 +10,37 @@ describe('HelloWorld.vue', () => {
     expect(wrapper.text()).toMatch(msg)
   })
 
-  it('HelloWorld should have h1, select with options, button, and p', () => {
+  it('HelloWorld should have h1, select with options', () => {
     const wrapper = shallowMount(HelloWorld);
     expect(wrapper.find('h1').exists()).toBeTruthy();
     expect(wrapper.find('select').exists()).toBeTruthy();
 
     const selectOptions = wrapper.findAll('select option');
-    expect(selectOptions.length).toBe(wrapper.vm.subjects.length);
 
-    wrapper.vm.subjects.forEach((subject:string, index:number) => {
-      expect(selectOptions[index].text()).toBe(subject);
+
+      expect(selectOptions.length).toBe(wrapper.vm.subjects.length + 1);
+      wrapper.vm.subjects.forEach((subject:string, index:number) => {
+        expect(selectOptions[index + 1].text()).toBe(subject);
+      });
+    
+
+  })
+
+  it('It shoul redirect to a selected subject or home page', () => {
+    const selectedSubject = 'Vue';
+    const routerMock = {
+      push: jest.fn(),
+    };
+
+    const wrapper = shallowMount(HelloWorld, {
+      global: {
+        provide: {
+          router: routerMock,
+        },
+      },
     });
 
-    expect(wrapper.find('button').exists()).toBeTruthy();
-    expect(wrapper.find('button').text()).toBe('Submit');
-    expect(wrapper.find('p').exists()).toBeTruthy();
+    wrapper.find('select').setValue(selectedSubject);
+    expect(routerMock.push).toHaveBeenCalledWith(`/${selectedSubject}`);
   })
 })
