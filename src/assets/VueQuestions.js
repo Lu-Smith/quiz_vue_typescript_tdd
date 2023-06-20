7d23550a9291365bdc116ca248dab0c9b27bddd4
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-06-20 12:05:00 +0100

    Created VueQuestions.js

---
+++ src/assets/VueQuestions.js
@@ -0,0 +1,84 @@
+const vueQuestions = [
+    {
+        "question": "What is Vue.js",
+        "answer": 0,
+        "options": [
+            "An open-source JavaScript framework.",
+            "A CSS preprocessor.",
+            "A programming language.",
+            "A database management system."
+        ]
+    },
+    {
+        "question": "Vue was develpoed by",
+        "answer": 0,
+        "options": [
+            "Evan You",
+            "Brendan Eich",
+            "Mark Zuckerberg",
+            "Jeff Bezos"
+        ]
+    },
+    {
+        "question": "Vue is based on",
+        "answer": 0,
+        "options": [
+            "MVVM (Model-View-ViewModel) pattern",
+            "MVC (Model-View-Controller) pattern",
+            "Observer pattern",
+            "Singleton pattern"
+        ]
+    },
+    {
+        "question": "How do you implement two-way data binding in a Vue.js component?",
+        "answer": 0,
+        "options": [
+            "Use the v-model directive.",
+            "Use the v-bind directive.",
+            "Use the v-on directive.",
+            "Use the v-show directive."
+        ]
+    },
+    {
+        "question": "What are computed properties in Vue.js",
+        "answer": 0,
+        "options": [
+            "Properties that are calculated based on other properties in the component.",
+            "Properties defined in data objects.",
+            "Properties used for event handling.",
+            "Properties used for styling."
+        ]
+    },
+    {
+        "question": "Features of Vue.js are?",
+        "answer": 0,
+        "options": [
+            "Templates, Reactivity, Components, Transitions, Routing ",
+            "Promises, Observables, Generators, Async/await",
+            "Inheritance, Polymorphism, Encapsulation, Abstraction",
+            "Dependency Injection, Inversion of Control, Aspect-Oriented Programming"
+        ]
+    },
+    {
+        "question": "Name directives in Vue.js?",
+        "answer": 0,
+        "options": [
+            "v-show, v-if, v-model, v-else, v-on ",
+            "v-for, v-bind, v-text, v-html, v-once",
+            "v-component, v-transition, v-slot, v-pre, v-cloak",
+            "v-validate, v-scroll, v-resize, v-model-sync, v-clipboard"
+        ]
+    },
+    {
+        "question": "What year Vue.js was realised?",
+        "answer": 0,
+        "options": [
+            "2014",
+            "2003",
+            "1994",
+            "2020",
+        ]
+    }
+]
+
+export default vueQuestions;
