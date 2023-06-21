9eb3488c55bf744a1f86990e9527152cccb8fbfe
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-06-21 13:03:47 +0100

    Updated VueQuestions.js

--- src/assets/VueQuestions.js
+++ src/assets/VueQuestions.js
@@ -119,7 +119,106 @@ const vueQuestions = [
             "Loop()",
         ],
         "selected": null
-    }
+    },
+    {
+        "question": "What is the virtual DOM in Vue.js?",
+        "answer": 1,
+        "options": [
+          "A representation of the actual DOM in memory.",
+          "A JavaScript object used for efficient rendering.",
+          "A concept unrelated to Vue.js.",
+          "A database management system."
+        ],
+        "selected": null
+      },
+      {
+        "question": "What is the purpose of the 'v-for' directive in Vue.js?",
+        "answer": 0,
+        "options": [
+          "To render a list of items based on an array.",
+          "To bind a value to an HTML attribute.",
+          "To conditionally show or hide an element.",
+          "To handle user input events."
+        ],
+        "selected": null
+      },
+      {
+        "question": "What is the difference between 'v-if' and 'v-show' directives in Vue.js?",
+        "answer": 1,
+        "options": [
+          "There is no difference, they can be used interchangeably.",
+          "'v-if' removes the element from the DOM, while 'v-show' hides it with CSS.",
+          "'v-if' hides the element with CSS, while 'v-show' removes it from the DOM.",
+          "'v-if' is used for rendering lists, while 'v-show' is used for conditional rendering."
+        ],
+        "selected": null
+      },
+      {
+        "question": "How can you listen to an event in Vue.js?",
+        "answer": 2,
+        "options": [
+          "Using the 'v-bind' directive.",
+          "Using the 'v-once' directive.",
+          "Using the 'v-on' directive.",
+          "Using the 'v-model' directive."
+        ],
+        "selected": null
+      },
+      {
+        "question": "What is the purpose of the 'watch' option in a Vue.js component?",
+        "answer": 3,
+        "options": [
+          "To define computed properties.",
+          "To handle user input events.",
+          "To create reactive data properties.",
+          "To react to changes in data properties or computed properties."
+        ],
+        "selected": null
+      },
+      {
+        "question": "How can you import a component in Vue.js?",
+        "answer": 0,
+        "options": [
+          "Using the 'import' statement in your script.",
+          "By adding a <component> tag in your HTML.",
+          "By using the 'v-import' directive.",
+          "By including a <script> tag with the component's URL."
+        ],
+        "selected": null
+      },
+      {
+        "question": "What is the purpose of the 'v-bind' directive in Vue.js?",
+        "answer": 1,
+        "options": [
+          "To conditionally show or hide an element.",
+          "To bind a value to an HTML attribute.",
+          "To render a list of items based on an array.",
+          "To handle user input events."
+        ],
+        "selected": null
+      },
+      {
+        "question": "What is the lifecycle hook used when a Vue.js component is created?",
+        "answer": 2,
+        "options": [
+          "beforeCreate()",
+          "created()",
+          "mounted()",
+          "destroyed()"
+        ],
+        "selected": null
+      },
+      {
+        "question": "How can you pass data from a parent component to a child component in Vue.js?",
+        "answer": 0,
+        "options": [
+            "Using props.",
+            "Using slots.",
+            "Using emits.",
+            "Using refs."
+        ],
+        "selected": null
+      },
 ]
 
 export default vueQuestions;
