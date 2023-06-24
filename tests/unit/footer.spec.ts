1000120e0f039c94a43ec68ade28b3523ab808a4
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-06-24 23:36:06 +0100

    Updated test unit for footer

--- tests/unit/footer.spec.ts
+++ tests/unit/footer.spec.ts
@@ -9,5 +9,13 @@ describe('MainFooter', () => {
         expect(footerElement.exists()).toBe(true);
 
         expect(footerElement.text()).toContain('Coded by');
+
+        const portfolioLink = footerElement.find('a[href="https://www.lunasmithart.com/"]');
+        expect(portfolioLink.exists()).toBe(true);
+        expect(portfolioLink.text()).toBe('Luna Smith');
+
+        const gitHubLink = footerElement.find('a[href="https://github.com/Lu-Smith/quiz_vue_typescript_tdd" ]');
+        expect(gitHubLink.exists()).toBe(true);
+        expect(gitHubLink.text()).toBe('GitHub');
     })
 }) 
