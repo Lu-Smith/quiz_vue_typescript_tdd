f2d7261359bb8529b6781a62072ca109d6cc497c
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-06-24 23:19:38 +0100

    Created unit test for App

---
+++ tests/unit/app.spec.ts
@@ -0,0 +1,13 @@
+import { shallowMount } from "@vue/test-utils";
+import App from '../../src/App.vue';
+import MainFooter from "@/components/MainFooter.vue";
+import HelloWorld from "@/components/HelloWorld.vue";
+
+describe('App', () => {
+    it('renders HelloWorld and MainFooter components', ()=> {
+        const wrapper = shallowMount(App);
+        expect(wrapper.findComponent(HelloWorld).exists()).toBe(true);
+        expect(wrapper.findComponent(MainFooter).exists()).toBe(true);
+
+    })
+})
