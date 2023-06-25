f7295041f329aa557cb15ca50d90f2ddd0e501d3
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-06-25 23:31:28 +0100

    Updated testing

--- tests/unit/mainQuiz.spec.ts
+++ tests/unit/mainQuiz.spec.ts
@@ -2,18 +2,18 @@ import { shallowMount } from "@vue/test-utils";
 import MainQuiz from "@/components/MainQuiz.vue";
 
 describe('MainQuiz', () => {
-    it('renders the component with correct content and functionality', () => {
-        const selectedSubject = 'Vue';
-        const quizCompleted = false;
+    it('renders the component with correct content and functionality, when quizCompleted = false', () => {
+
         const questionsArray = [{ question: 'Question 1', answer: 1, options: ['Option 1', 'Option 2', 'Option 3', 'Option 4'], selected: null }];
         const currentQuestion = 0;
+        const selectedSubject = 'Vue';
 
         const wrapper = shallowMount(MainQuiz, {
-            props: { selectedSubject, quizCompleted, questionsArray, currentQuestion},
+            props: {questionsArray, currentQuestion, selectedSubject, quizCompleted: false },
         });
 
-        if(!quizCompleted) {
             expect(wrapper.find('[data-test="quiz-title"]').exists()).toBe(true);
+            expect(wrapper.find('[data-test="quiz-title"]').text()).toBe(selectedSubject + ' quiz');
             const scoreElement = wrapper.find('[data-test="score-container"]');
             expect(scoreElement.exists()).toBe(false);
             const quizElement = wrapper.find('div.quiz');
@@ -29,12 +29,33 @@ describe('MainQuiz', () => {
                     expect(spanElement.exists()).toBe(true);
                 const buttonElement = quizElement.find('button');
                 expect(buttonElement.exists()).toBe(true);
-        } else {
-            expect(wrapper.find('[data-test="quiz-title"]').exists()).toBe(false);
-            const scoreElement = wrapper.find('[data-test="score-container"]');
-            expect(scoreElement.exists()).toBe(true);
-            const quizElement = wrapper.find('div.quiz');
-            expect(quizElement.exists()).toBe(false);
-        }
+
     })
+
+
+    // it('renders the component with correct content and functionality, when quizCompleted = true', () => {
+   
+    //     const questionsArray = [{ question: 'Question 1', answer: 1, options: ['Option 1', 'Option 2', 'Option 3', 'Option 4'], selected: null }];
+    //     const currentQuestion = 0;
+  
+  
+    //         const wrapper = shallowMount(MainQuiz, {
+    //             props: { questionsArray, currentQuestion, quizCompleted: true }
+
+    //         });
+    
+    //             expect(wrapper.find('[data-test="quiz-title"]').exists()).toBe(false);
+    //             const scoreElement = wrapper.find('[data-test="score-container"]');
+    //             expect(scoreElement.exists()).toBe(true);
+    //             const finalScore = scoreElement.find('p');
+    //             expect(finalScore.exists()).toBe(true); 
+    //             const finalButtonElement = scoreElement.find('button.playAgain');
+    //             expect(finalButtonElement.exists()).toBe(true); 
+    //             const messageElement = scoreElement.find('h3');
+    //             expect(messageElement.exists()).toBe(true);
+    
+    //             const quizElement = wrapper.find('div.quiz');
+    //             expect(quizElement.exists()).toBe(false);
+        
+    // })
 })
