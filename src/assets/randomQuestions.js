7f16743916ac41271a9245120c47f3ddcfeedfef
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-11-22 22:09:03 +0000

    Updated randomQuestions.js

--- src/assets/randomQuestions.js
+++ src/assets/randomQuestions.js
@@ -9,6 +9,11 @@ const randomQuestions = [
     "explanation": "Markup language and programming language are two distinct types of languages used in different domains and serve different purposes:\n\nMarkup Language:\n\nMarkup languages are used to annotate and format text to provide structure and presentation.\n\nMarkup languages are not programming languages but rather descriptive languages.\n\nThey define the structure and appearance of documents and are primarily used for displaying information.\n\nExamples of markup languages include HTML (Hypertext Markup Language), XML (eXtensible Markup Language), Markdown, and LaTeX.\n\nMarkup languages focus on describing the document's content and how it should be displayed or structured.\n\nProgramming Language:\n\nProgramming languages are used to write executable instructions that control the behavior of a computer program.Programming languages are used for creating algorithms, implementing logic, manipulating data, and building software applications.\n\nThey provide the ability to define variables, control flow, perform calculations, and interact with system resources.\n\nExamples of programming languages include JavaScript, Python, Java, C++, Ruby, and many others.\n\nProgramming languages are designed to express computations and algorithms in a way that can be executed by a computer.\n\n\n\nIn summary, markup languages are used for structuring and presenting information, while programming languages are used for creating executable instructions and building software applications. Markup languages focus on presentation and structure, whereas programming languages focus on computational logic and automation.\n\n",
     "selected": null
   },
+  {
+    "question": "What is Data Binding?",
+    "explanation": "Data binding is a concept in software development that establishes a connection between the application's data (data source) and the user interface (UI) elements. Two common types of data binding are one-way data binding (from data source to UI) and two-way data binding (from data source to UI, and from UI to data source, ex v-model in Vue)",
+    "selected": null
+  },
 ];
 
 export default randomQuestions
