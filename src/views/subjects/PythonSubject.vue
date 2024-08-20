ad39e5747b9e7df3eed004e0d23aec30165ffaa3
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-08-20 17:19:05 +0100

    Created PythonSubject.vue

---
+++ src/views/subjects/PythonSubject.vue
@@ -0,0 +1,22 @@
+<template>
+    <h2>{{ selectedSubject }}</h2>
+    <MainQuiz :selectedSubject="selectedSubject" :questionsArray="questionsArray"/>
+  </template>
+  
+  <script lang="ts">
+  import pythonQuestions from '../../assets/pythonQuestions.js'
+  import { ref } from 'vue'
+  import MainQuiz from '../../components/MainQuiz.vue'
+  
+  export default {
+    components: {
+      MainQuiz
+    },
+    data() {
+      return {
+        selectedSubject: ref('Python'),
+        questionsArray: pythonQuestions
+      }
+    }
+    }
+  </script>
